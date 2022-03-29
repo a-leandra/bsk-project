a8f321dda0c6b74e585de2cfd9ba64130d6330ad
a-leandra <ola.nadzieja@wp.pl>
2022-03-29 12:11:42 +0200

    add gui-chat

--- client.py
+++ client.py
@@ -1,64 +1,91 @@
 import socket
 import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
 
 import config
 
-nickname = input("Choose a nickname: ")
-if nickname == 'admin':
-    password = input("Enter password for admin: ")
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((config.HOST, config.PORT))
+class Client:
 
-stop_thread = False
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
+
+        self.gui_done = False
+
+        self.running = True
+
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightgray")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(config.PACKAGE_SIZE).decode('utf-8')
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
 
 
-def receive():
-    while True:
-        global stop_thread
-        if stop_thread:
-            break
-        try:
-            message = client.recv(config.PACKAGE_SIZE).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
-                next_message = client.recv(config.PACKAGE_SIZE).decode('utf-8')
-                if next_message == 'PASS':
-                    client.send(password.encode('utf-8'))
-                    if client.recv(config.PACKAGE_SIZE).decode('utf-8') == 'REFUSE':
-                        print("Connection was refused! Wrong password!")
-                        stop_thread = True
-            elif next_message == 'BAN':
-                print("Connection refused because of ban!")
-                client.close()
-                stop_thread = True
-            else:
-                print(message)
-        except:
-            print("An error occurred!")
-            client.close()
-            break
-
-
-def write():
-    while True:
-        if stop_thread:
-            break
-        message = f'{nickname}: {input("")}'
-        if message[len(nickname) + 2:].startswith('/'):
-            if nickname == 'admin':
-                if message[len(nickname) + 2:].startswith('/kick'):
-                    client.send(f'KICK {message[len(nickname) + 2 + 6:]}'.encode('utf-8'))
-                elif message[len(nickname) + 2:].startswith('/ban'):
-                    client.send(f'BAN {message[len(nickname) + 2 + 5:]}'.encode('utf-8'))
-            else:
-                print('Commands can only be executed by the admin!')
-        else:
-            client.send(message.encode('utf-8'))
-
-
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+client = Client(config.HOST, config.PORT)
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-import threading
 import socket
+import threading
+
 import config
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((config.HOST, config.PORT))
+
 server.listen()
 
 clients = []
@@ -17,35 +19,17 @@ def broadcast(message):
 
 def handle(client):
     while True:
+        index = clients.index(client)
         try:
-            msg = message = client.recv(config.PACKAGE_SIZE)
-            if msg.decode('utf-8').startswith('KICK'):
-                if nicknames[clients.index(client)] == 'admin':
-                    name_to_kick = msg.decode('utf-8')[5:]
-                    kick_user(name_to_kick)
-                else:
-                    client.send('Command was refused!'.encode('utf-8'))
-            elif msg.decode('utf-8').startswith('BAN'):
-                if nicknames[clients.index(client)] == 'admin':
-                    name_to_ban = msg.decode('utf-8')[4:]
-                    if name_to_ban != 'admin':
-                        kick_user(name_to_ban)
-                        with open('bans.txt', 'a+') as f:
-                            f.write(f'{name_to_ban}\n')
-                        print(f'{name_to_ban} was banned!')
-                else:
-                    client.send('Command was refused!'.encode('utf-8'))
-            else:
-                broadcast(message)
+            message = client.recv(config.PACKAGE_SIZE)
+            print(f"{nicknames[index]} says {message}")
+            broadcast(message)
         except:
-            if client in clients:
-                index = clients.index(client)
-                clients.remove(client)
-                client.close()
-                nickname = nicknames[index]
-                broadcast(f'{nickname} left the chat!'.encode('utf-8'))
-                nicknames.remove(nickname)
-                break
+            clients.remove(client)
+            client.close
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
 
 
 def receive():
@@ -54,44 +38,18 @@ def receive():
         print(f"Connected with {str(address)}")
 
         client.send('NICK'.encode('utf-8'))
-        nickname = client.recv(config.PACKAGE_SIZE).decode('utf-8')
-
-        with open('bans.txt', 'r') as f:
-            bans = f.readlines()
-
-        if nickname + '\n' in bans:
-            client.send('BAN'.encode('utf-8'))
-            client.close()
-            continue
-
-        if nickname == 'admin':
-            client.send('PASS'.encode('utf-8'))
-            password = client.recv(config.PACKAGE_SIZE).decode('utf-8')
-
-            if password != 'admin':
-                client.send('REFUSE'.encode('utf-8'))
-                client.close()
-                continue
+        nickname = client.recv(config.PACKAGE_SIZE)
 
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f'Nickname of the client is {nickname}')
-        broadcast(f'{nickname} joined the chat!'.encode('utf-8'))
-        client.send('Connected to the server!'.encode('utf-8'))
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} joined the chat!\n".encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-def kick_user(name):
-    if name in nicknames and name != 'admin':
-        name_index = nicknames.index(name)
-        client_to_kick = clients[name_index]
-        clients.remove(client_to_kick)
-        client_to_kick.send('You were kicked by an admin!'.encode('utf-8'))
-        client_to_kick.close()
-        nicknames.remove(name)
-        broadcast(f'{name} was kicked by an admin!'.encode('utf-8'))
 
-print("Server is listening...")
+print("Server is listening..")
 receive()
