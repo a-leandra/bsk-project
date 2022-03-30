8c60f78310ef01b09dd217a9f19c5301da96ca0a
a-leandra <ola.nadzieja@wp.pl>
2022-03-30 19:08:39 +0200

    add simple UDP client-server

--- server.py
+++ server.py
@@ -1,55 +1,10 @@
 import socket
-import threading
 
 import config
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((config.HOST, config.PORT))
 
-server.listen()
-
-clients = []
-nicknames = []
-
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-
-def handle(client):
-    while True:
-        index = clients.index(client)
-        try:
-            message = client.recv(config.PACKAGE_SIZE)
-            print(f"{nicknames[index]} says {message}")
-            broadcast(message)
-        except:
-            clients.remove(client)
-            client.close
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
-            break
-
-
-def receive():
-    while True:
-        client, address = server.accept()
-        print(f"Connected with {str(address)}")
-
-        client.send('NICK'.encode('utf-8'))
-        nickname = client.recv(config.PACKAGE_SIZE)
-
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f"Nickname of the client is {nickname}")
-        broadcast(f"{nickname} joined the chat!\n".encode('utf-8'))
-        client.send("Connected to the server".encode('utf-8'))
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
-
-print("Server is listening..")
-receive()
+message, address = server.recvfrom(config.PACKAGE_SIZE)
+print(message.decode('utf-8'))
+server.sendto("Hello Client!".encode('utf-8'), address)
